93f1ce9c1563ffd18fdda146664a14ff1faa5b86
Nick Lima <nickdigietal@gmail.com>
2017-04-21 11:05:20 -0300

    update jeet version

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 //Esse STARTER GULP + JEET é baseado no HARVEST (http://www.ryanbensonmedia.com/harvest) de Ryan Benson
 //Esse GULPFILE foi desenvolvido por NICHOLAS LIMA E ALLEF BRUNO
 //Qualquer dúvida ou sugestão, envie um e-mail para nick.lima.wp@gmail.com ou allefbr21@gmail.com
+//Diretorio DEV  : ./src
+//Diretorio PROD : /assets
 
-var gulp, gutil, browserSync, sass, autoprefixer, minifyCSS, uglify, concat, imagemin, changed, path;
+'use strict';
+var gulp, path, sass, cssnano, sourcemaps, concat, uglify, browserSync, browsersup, gutil, imagemin, spritesmith, changed, gulpif, sprity;
 
 //Diretorios
 path = {
-    dev     : './src',
+    dev     : './_src',
     prod    : './assets',
+    // proxy   : 'http://localhost/jeet'
 }
 
 //Carregamento de modulos
@@ -15,12 +19,30 @@ gulp            = require('gulp');
 gutil           = require('gulp-util');
 browserSync     = require('browser-sync').create();
 sass            = require('gulp-sass');
-autoprefixer    = require('gulp-autoprefixer');
-minifyCSS       = require('gulp-minify-css');
 uglify          = require('gulp-uglify');
-concat          = require('gulp-concat');
 imagemin        = require('gulp-imagemin');
+
+sprity          = require('sprity');
+spritesmith     = require("gulp-spritesmith");
 changed         = require('gulp-changed');
+gulpif          = require("gulp-if");
+
+sourcemaps      = require('gulp-sourcemaps');
+concat          = require('gulp-concat');
+cssnano         = require('gulp-cssnano');
+browsersup      = [
+    'Android >= 2.3',
+    'BlackBerry >= 7',
+    'Chrome >= 9',
+    'Firefox >= 4',
+    'Explorer >= 9',
+    'iOS >= 5',
+    'Opera >= 11',
+    'Safari >= 5',
+    'ChromeAndroid >= 9',
+    'FirefoxAndroid >= 4',
+    'ExplorerMobile >= 9'
+];
 
 // ================================================
 // TASKS
@@ -32,33 +54,36 @@ gulp.task('sinc', function() {
       server: {
         baseDir: "./"
       },
+      //Para acessar o localhost, remova o comentário da opção 'proxy: path.proxy' e comente a opção 'server: {baseDir: "./"}'
       //proxy: path.proxy,
       options: {
         reloadDelay: 250
       },
-      notify: true,      
+      notify: true,
   });
 });
 
-//Compila arquivos scss/sass para css 
+//Compila arquivos scss/sass para css
 gulp.task('sass', function() {
     //Arquivo SCSS principal que deve importar todos os outros
     return gulp.src( path.dev + '/scss/main.scss')
-        //local do arquivo SCSS do ambiente de desenvolvimento e suas subpastas
-        .pipe(sass({
-              errLogToConsole: false,
-              includePaths: [
-                   path.dev + '/scss/'
-              ]
-        }))                
-        //nome do arquivo css finalizado
-        .pipe(concat('main.min.css'))
-        //comprimindo...
-        .pipe(minifyCSS())        
-        //where to save our final, compressed css file
-        .pipe(gulp.dest( path.prod + '/css'))
-        //notifica o browserSync para atualizar
-        .pipe(browserSync.reload({stream: true}));
+    //local do arquivo SCSS do ambiente de desenvolvimento e suas subpastas
+    .pipe(sass({
+          sourcemap: true,
+          outputStyle: 'expanded',
+          includePaths: [
+               path.dev + '/scss/**/*'
+          ]
+
+    }))
+    .pipe(cssnano({autoprefixer: {browsers: browsersup, add: true}}))
+    .pipe(sourcemaps.write())
+    //nome do arquivo css finalizado
+    .pipe(concat('main.min.css'))
+    //local para salvar o CSS final
+    .pipe(gulp.dest( path.prod + '/css'))
+    //notifica o browserSync para atualizar
+    .pipe(browserSync.reload({stream: true}));
 });
 
 //compiling our Javascripts
@@ -79,9 +104,9 @@ gulp.task('scripts', function() {
 gulp.task('images', function() {
     return gulp.src(path.dev + '/images/*')
         .pipe(changed( path.prod + '/images')).pipe(
-            imagemin({ 
+            imagemin({
                 optimizationLevel: 5,
-                progressive: true, 
+                progressive: true,
                 interlaced: true,
                 svgoPlugins: [{removeViewBox: false}]
                 }))
@@ -90,7 +115,7 @@ gulp.task('images', function() {
 
 //Task default
 gulp.task('default', ['sinc', 'scripts', 'images', 'sass'], function(){
-    gulp.watch( path.dev + '/scss/**/*.scss', ['sass']);    
+    gulp.watch( path.dev + '/scss/**/*.scss', ['sass']);
     gulp.watch( path.dev + '/js/*.js', ['scripts']);
     gulp.watch( path.dev + '/images/*', ['images']);
 });
